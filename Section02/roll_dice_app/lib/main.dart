8e666a9fa00083a6d0094f33edb9a70eef36cbab
robin-off <john.robin.davis.it@gmail.com>
2026-01-04 19:30:45 +0530

    Decorated the app

--- Section02/roll_dice_app/lib/main.dart
+++ Section02/roll_dice_app/lib/main.dart
@@ -3,7 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purpleAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
     ),
   );
 }
