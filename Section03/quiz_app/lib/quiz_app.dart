e33b66dffb8c8cd1253205f9bb5d2dd328742c5c
robin-off <john.robin.davis.it@gmail.com>
2026-01-06 13:38:39 +0530

    Added main screen

---
+++ Section03/quiz_app/lib/quiz_app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      home: Scaffold(
+        backgroundColor: Colors.purple,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                height: 300,
+                width: 300,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.deepPurpleAccent,
+                ),
+                child: Text('Start Quiz'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
