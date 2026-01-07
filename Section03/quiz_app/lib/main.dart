5a89013e5751d7723ffde308f792219b7a3864fd
robin-off <john.robin.davis.it@gmail.com>
2026-01-07 11:07:47 +0530

    Synchronized code with the course

--- Section03/quiz_app/lib/main.dart
+++ Section03/quiz_app/lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/quiz_app.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const QuizApp());
+  runApp(
+    MaterialApp(
+      title: 'Quiz App',
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
---
+++ Section03/quiz_app/lib/start_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(50, 255, 255, 255),
+          ),
+          SizedBox(height: 80),
+          Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: const Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            label: Text('Start Quiz'),
+            icon: Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
+    );
+  }
+}
