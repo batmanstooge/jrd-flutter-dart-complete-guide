347033e131c8898f4a62cca06c6b5d0a481b8063
robin-off <john.robin.davis.it@gmail.com>
2026-01-09 11:53:51 +0530

    Shuffled answers

--- Section03/quiz_app/lib/models/quiz_question.dart
+++ Section03/quiz_app/lib/models/quiz_question.dart
@@ -2,5 +2,11 @@ class QuizQuestion {
   final String questionText;
   final List<String> answers;
 
+  List<String> getShuffledAnswers() {
+    final shuffledAnswers = List<String>.from(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
+
   const QuizQuestion(this.questionText, this.answers);
 }
